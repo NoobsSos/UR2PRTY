42168cce4b241bdbd46f4136e3cee47b6504ee28
Nazar M <nazar.mraka@gmail.com>
2023-03-07 08:34:07 +0200

    fix create bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,12 +1,13 @@
 const Bootcamp = require('../models/Bootcamp');
+const asyncHandler = require('../middleware/async');
 
 
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps', hello: req.hello });
-}
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    res.status(200).json(res.advancedResults);
+});
 
 // @desc        Get single bootcamps
 // @route       GET /api/v1/bootcamps/:id
@@ -18,14 +19,14 @@ exports.getBootcamp = (req, res, next) => {
 // @desc        Create bootcamps
 // @route       POST /api/v1/bootcamps
 // @access      Private
-exports.createBootcamp = async (req, res, next) => {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
 
     res.status(201).json({
       success: true,
       data: bootcamp
     });
-}
+  });
 
 // @desc        Update bootcamps
 // @route       PUT /api/v1/bootcamps/:id
